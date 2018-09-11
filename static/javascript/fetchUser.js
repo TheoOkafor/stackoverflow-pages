612bb08f6d59140b232a75fe82704c410a2b5f5c
TheoOkafor <theo.okafor@yahoo.com>
2018-09-11 09:25:37 +0100

    [feature #160136927] Modify fetch user profile page

--- static/javascript/fetchUser.js
+++ static/javascript/fetchUser.js
@@ -4,7 +4,7 @@ const allQuestionNum = document.getElementById('all-questions');
 const profile = document.getElementById('profile-summary');
 const table = document.getElementsByClassName('table')
 const profileName = document.getElementsByClassName('profile-username');
-const url = 'http://localhost:3000/v1/users/1';
+const url = `http://localhost:3000/v1${location.pathname}`;
 
 const fetchUser = () => {
 	
@@ -13,44 +13,65 @@ const fetchUser = () => {
 			return response.json();
 		})
 		.then ( result => {
-			console.log(result);
-			userDetails = result.data[0][0];
-			//userQuestionsArr is an array
-			userQuestionsArr = result.data[1];
-			//userAnswersArr is an array
-			userAnswersArr = result.data[2];
+			if (result.statusCode === 200) {
+				console.log(result);
+				let timeJoined = new Date(result.data.created).toUTCString();
+				// userQuestionsArr is the questions array
+				let userQuestionsArr = result.data.questions;
+				let userAnswersArr = result.data.answers;
 
-			for(let i=0; i<profileName.length; i++){
-				profileName[i].innerHTML = userDetails.username;
-			}
-			allQuestionNum.innerHTML = userQuestionsArr.length;
-
-			const profileSummary = `
-					<h4 class="margin-bottom-20"><span class="profile-username">
-							${userDetails.username}</span>'s Summary</h4>
-
-					<ul class="list list-unstyled">
-						<li>Joined: <b>${userDetails.created}</b></li>
-						<li><b>${userQuestionsArr.length}</b> Questions</li>
-						<li><b>${userAnswersArr.length}</b> Answers</li>
-					</ul>
-			`
-			profile.innerHTML = profileSummary; 
-
-			userQuestionsArr.map( question => {
-				const recentQuestion = `
-						<td><p><a href="question.html" class="question">
-							${question.title}</a></p></td>
-						<td><p class="num-answer">${question.id}</p></td>
-				`;
-
-				for (let i=0; i<table.length; i++){
-					let tr = document.createElement('tr');
-					tr.innerHTML = recentQuestion;
-					table[i].appendChild(tr);
+				//Loops through the profileName DOM object filling it with the username 
+				for(let i=0; i<profileName.length; i++){
+					profileName[i].innerHTML = result.data.username;
 				}
+
+				allQuestionNum.innerHTML = userQuestionsArr.length;
+
+				const profileSummary = `
+						<h4 class="margin-bottom-20"><span class="profile-username">
+								${result.data.username}</span>'s Summary</h4>
+
+						<ul class="list list-unstyled">
+							<li>Joined: <b>${timeJoined}</b></li>
+							<li><b>${userQuestionsArr.length}</b> Questions</li>
+							<li><b>${userAnswersArr.length}</b> Answers</li>
+						</ul>
+				`
+				profile.innerHTML = profileSummary; 
+
+				userQuestionsArr.map( question => {
+
+					let numAnswers = question.answers.length;
+					// Get an array containing the accepted answer
+					let acceptedAnswer = question.answers.filter( answer => {
+						if (answer.accepted) {
+							return true;
+						}
+					});
+
+					// Check whether the question has an answer that has been accepted.
+					let hasAccepted = acceptedAnswer.length > 0? true: false;
+
+					const recentQuestion = `
+							<td><p><a href="${location.href.split('/')[0]}/questions/${question.id}" 
+								class="question">
+								${question.title}</a></p></td>
+							<td><p class="num-answer ${hasAccepted?'selected': ''}">${numAnswers}</p></td>
+					`;
+
+					for (let i=0; i<table.length; i++){
+						let tr = document.createElement('tr');
+						tr.innerHTML = recentQuestion;
+						table[i].appendChild(tr);
+					}
+				})
+
+				} else {
+					location.assign(`${location.href.split('/')[0]}/error-404`);
+				}
+			})
+			.catch(error => {
+				console.log(error);
 			});
-			
-		})
 
 }
