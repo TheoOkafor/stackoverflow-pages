e01f3689a40a528f295dfad05c2bf08f3f8210e0
TheoOkafor <theo.okafor@yahoo.com>
2018-09-11 22:41:57 +0100

    [feature #160420269] Create upvote/downvote and top questions fetch scripts

---
+++ static/javascript/fetchTopQuestions.js
@@ -0,0 +1,63 @@
+const fetchTopQuestions = () => {
+
+	const topQuestions = document.getElementById('top-questions');
+	let url = `http://localhost:3000/v1/questions`
+	fetch (url)
+		.then( response => {
+			return response.json();
+		})
+		.then ( result => {
+			console.log(result);
+			let questionSummary = [];
+			let hasAccepted;
+			let data =result.data.slice(0).reverse();
+
+			//creates an div element called card and maps the questions to it.
+			data.map( question => {
+				let i = 0;
+				let answer = '';
+				let time = '';
+				let username = '';
+				let currAnswers = question.answers;
+				let numAnswers = question.numAnswers || 0;
+
+				//Makes an array of only accepted answer
+				let acceptedAns = currAnswers.filter(answer => {
+					if(answer.accepted){
+						return true;
+					}
+				})
+				if (acceptedAns.length > 0){
+						hasAccepted = true;
+				} else {
+						hasAccepted = false;
+				}
+
+				questionSummary.push({
+					id: question.id,
+					numAnswers: question.numAnswers,
+					title: question.title,
+					hasAccepted: hasAccepted,
+				});		
+			});
+
+			
+			questionSummary.sort((a, b) => { 
+				return b.numAnswers - a.numAnswers});
+			
+			for (let i=0; i < 6; i++) {
+				let tableItem = document.createElement('tr');
+				tableItem.innerHTML = `
+					<td><p class="num-answer ${questionSummary[i].hasAccepted?'selected': ''}">
+						${questionSummary[i].numAnswers}</p></td>
+					<td><p><a href="${location.href.split('/')[0]}/questions/${questionSummary[i].id}">
+						${questionSummary[i].title}</a></p></td>
+`;
+				topQuestions.appendChild(tableItem);
+			}
+		})
+		.catch(error => {
+			console.log(error);
+		});
+
+};
---
+++ static/javascript/postVote.js
@@ -0,0 +1,70 @@
+// UPVOTE ANSWER
+const upvoteAnswer = (id) => {
+  // const voteDisp = document.getElementsByClassName('vote-display');
+  const voteMssgDisp = document.getElementById(`vote-mssg-${id}`);
+  const link = `http://localhost:3000/v1${location.pathname}/answers/${id}`;
+  const token = window.localStorage.getItem('x-access-token');
+
+  fetch (link, {
+    method: 'post',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'x-access-token': token
+    }),
+    body: JSON.stringify({
+      'vote': 'upvote' 
+    }),
+  })
+  .then( (response) => {
+    return response.json();
+  })
+  .then( (result) => {
+    if (result.statusCode === 201) {
+      voteMssgDisp.setAttribute('class', 'text-success');
+      voteMssgDisp.innerHTML = result.message;
+      setTimeout(location.reload(true), 3000); //Reload the page from server
+    } else {
+      voteMssgDisp.setAttribute('class', 'text-danger');
+      voteMssgDisp.innerHTML = result.error;
+    }
+  })
+  .catch(error =>{
+    console.log(error);
+  });
+
+};
+
+// DOWNVOTE ANSWER
+const downvoteAnswer = (id) => {
+  const voteMssgDisp = document.getElementById(`vote-mssg-${id}`);
+  const link = `http://localhost:3000/v1${location.pathname}/answers/${id}`;
+  const token = window.localStorage.getItem('x-access-token');
+
+  fetch (link, {
+    method: 'post',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'x-access-token': token
+    }),
+    body: JSON.stringify({
+      'vote': 'downvote' 
+    }),
+  })
+  .then( (response) => {
+    return response.json();
+  })
+  .then( (result) => {
+    if (result.statusCode === 201) {
+      voteMssgDisp.setAttribute('class', 'text-success');
+      voteMssgDisp.innerHTML = result.message;
+      setTimeout(location.reload(true), 3000); //Reload the page from server
+    } else {
+      voteMssgDisp.setAttribute('class', 'text-danger');
+      voteMssgDisp.innerHTML = result.error;
+    }
+  })
+  .catch(error =>{
+    console.log(error);
+  });
+
+};
